93509d0321b53dea5b0502b164f6273deaf41e34
vendi <svitlana.vakulenko@gmail.com>
2018-07-03 23:57:07 +0200

    fix

--- data/data_generator.py
+++ data/data_generator.py
@@ -9,25 +9,25 @@ import random
 # load patterns
 with open('search_patterns.txt') as f:
     patterns = f.readlines()
-# load search terms
-with open('search_terms.txt') as f:
-    terms = f.readlines()
 
 random.shuffle(patterns)
 
 with open('generated_train.txt', 'wb') as f_train, open('generated_test.txt', 'wb') as f_test:
     for pattern in patterns:
-        # shuffle terms
-        random.shuffle(terms)
+        # load search terms
+        with open('search_terms.txt') as f:
+            terms = f.readlines()
+            # shuffle terms
+            random.shuffle(terms)
         # generate samples
         samples = []
-        for i in xrange(len(terms)):
+        while terms:
             sample = ''
             search_terms = []
             for c in pattern.strip():
                 if c == '*':
                     # draw a random search term
-                    term = terms[i].strip()
+                    term = terms.pop().strip()
                     search_terms.append(term)
                     sample += term
                 else:
@@ -35,7 +35,6 @@ with open('generated_train.txt', 'wb') as f_train, open('generated_test.txt', 'w
             sample = sample + '\tsearch ' + ' '.join(search_terms)
             print sample
             samples.append(sample)
-        assert len(samples) == len(terms)
 
         train_samples = samples[:40]
         f_train.writelines('\n'.join(train_samples) + '\n')
